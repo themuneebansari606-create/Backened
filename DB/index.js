88852057ea04768c9b38e4ea27e1cbc0b816a44d
themuneebansari606-create <themuneebansari606@gmail.com>
2025-08-12 01:17:39 +0500

    Update index.js

--- DB/index.js
+++ DB/index.js
@@ -3,7 +3,12 @@ import { configDotenv } from 'dotenv';
 configDotenv()
 const connectDB =async()=>{
     try{
-const connection = await mongoose.connect(process.env.mongodb_URL)
+const connection = await mongoose.connect(process.env.mongodb_URL,{
+     useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000,
+      socketTimeoutMS: 45000,
+})
 console.log("mongodb connected");
 
     }catch(error){
